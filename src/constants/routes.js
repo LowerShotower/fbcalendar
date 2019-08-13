c76440441fda407cb1f22bdf735676697fe244e1
Artsem_Zhgarou <lowershotower@gmail.com>
2019-08-13 12:27:22 +0300

    fix: github pages

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -1,8 +1,8 @@
 const routes = {
-  SIGN_UP: process.env.PUBLIC_URL + '/sign-up',
-  SIGN_IN: process.env.PUBLIC_URL + '/sign-in',
-  CALENDAR: process.env.PUBLIC_URL + '/calendar',
-  HOME: process.env.PUBLIC_URL + '/',
+  SIGN_UP: '' + process.env.PUBLIC_URL + '/sign-up',
+  SIGN_IN: '' + process.env.PUBLIC_URL + '/sign-in',
+  CALENDAR: '' + process.env.PUBLIC_URL + '/calendar',
+  HOME: '' + process.env.PUBLIC_URL + '/',
 };
 
 
