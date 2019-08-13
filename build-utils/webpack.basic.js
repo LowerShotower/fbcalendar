91f07cf1c6e44e4ea2873f4ee6355bf77c79c082
Artsem_Zhgarou <lowershotower@gmail.com>
2019-08-13 11:56:05 +0300

    docs: git pages

--- build-utils/webpack.basic.js
+++ build-utils/webpack.basic.js
@@ -2,6 +2,7 @@ const webpack = require('webpack');
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
 module.exports = {
   entry: ["@babel/polyfill", "./src/index.js"],
   module: {
--- src/constants/routes.js
+++ src/constants/routes.js
@@ -1,8 +1,10 @@
 const routes = {
-  SIGN_UP: '/sign-up',
-  SIGN_IN: '/sign-in',
-  CALENDAR: '/calendar',
-  HOME: '/',
+  SIGN_UP: process.env.PUBLIC_URL + '/sign-up',
+  SIGN_IN: process.env.PUBLIC_URL + '/sign-in',
+  CALENDAR: process.env.PUBLIC_URL + '/calendar',
+  HOME: process.env.PUBLIC_URL + '/',
 };
+
+
 
 export default routes;
